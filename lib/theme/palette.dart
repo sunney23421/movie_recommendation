22eafc865f0a06f41f49b5bb31b11c45d343828b
sunney <doglnw8@gmail.com>
2022-02-11 17:51:27 +0700

    theme

---
+++ lib/theme/palette.dart
@@ -0,0 +1,16 @@
+//Defi all the colors that we're going to use
+
+import 'package:flutter/rendering.dart';
+
+class Palette {
+  static const Color almostBlack = Color(0xff040B13);
+  static const Color red100 = Color(0xfffce5e3);
+  static const Color red200 = Color(0xfff7b2ba);
+  static const Color red300 = Color(0xfff27f8b);
+  static const Color red400 = Color(0xffec4b5f);
+  static const Color red500 = Color(0xffE82139);
+  static const Color red600 = Color(0xffb41326);
+  static const Color red700 = Color(0xff800d1b);
+  static const Color red800 = Color(0xff4b0810);
+  static const Color red900 = Color(0xff1a0305);
+}
